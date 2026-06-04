78a5babe2bafe8a39a887ae07b430e8aafa138a3
gurauionut-dev <gurauionut@gmail.com>
2026-06-04 11:06:19 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,9 @@
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBFE2W-kCKniS7xs7KSi7VhPi05oeOpa9A",
+  authDomain: "crew-tracker-led.firebaseapp.com",
+  projectId: "crew-tracker-led",
+  storageBucket: "crew-tracker-led.firebasestorage.app",
+  messagingSenderId: "40454072780",
+  appId: "1:40454072780:web:dae8a38f9569a18ffbd569"
+};
