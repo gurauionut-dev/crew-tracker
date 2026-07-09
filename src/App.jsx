a66b2cc6ebfcad528c5199c5b8a64b38b3dc9eaa
gurauionut-dev <gurauionut@gmail.com>
2026-07-09 20:58:49 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { db, saveChecked, saveApproval, listenChecked, listenApprovals, saveEventColor, listenEventColors } from "./firebase";
+import { doc, setDoc, collection, onSnapshot, serverTimestamp } from "firebase/firestore";
 import DevizeView from "./Devize";
 import RaportBusiness from "./Raport";
 import AvizeView from "./Avize";
